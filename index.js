5fb613338cf503464ded57b5b6534c2a9caa7f5d
Ryo Nakamaru <pottava@gmail.com>
2020-08-26 08:24:10 +0000

    Cloud Run demo scripts

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((_, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Yello!');
+});
+
+server.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${port}/`);
+});
